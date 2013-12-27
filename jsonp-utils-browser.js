5d86e31084ef2e56cb36bef73cc05350ee5a604c
Olivier Scherrer <pode.fr@gmail.com>
2013-12-27 17:07:19 -0500

    add built files

---
+++ jsonp-utils-browser.js
@@ -0,0 +1,10 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"o7y9Nn":[function(require,module,exports){
+/**
+ * jsonp.js - http://podefr.github.com/jsonp/
+ * Copyright(c) 2013 Olivier Scherrer <pode.fr@gmail.com>
+ * MIT Licensed
+ */
+"use strict";module.exports={};
+},{}],"jsonp-utils":[function(require,module,exports){
+module.exports=require('o7y9Nn');
+},{}]},{},[])
