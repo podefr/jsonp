8f36c4599d5e94637defa4d6077187fe1a2c28b3
Olivier Scherrer <pode.fr@gmail.com>
2013-12-27 17:00:38 -0500

    add license headers

--- js/jsonp-utils.js
+++ js/jsonp-utils.js
@@ -1,3 +1,8 @@
+/**
+ * jsonp-utils.js - http://podefr.github.com/jsonp/
+ * Copyright(c) 2013 Olivier Scherrer <pode.fr@gmail.com>
+ * MIT Licensed
+ */
 "use strict";
 
 module.exports = {
---
+++ test/jsonpSpec.js
@@ -0,0 +1,5 @@
+/**
+ * jsonpSpec.js - http://podefr.github.com/jsonp/
+ * Copyright(c) 2013 Olivier Scherrer <pode.fr@gmail.com>
+ * MIT Licensed
+ */
